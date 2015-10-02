07968f7641f3ee781f58d80a6ba237c1d61be52e
Istvan Demeter <demeter.istvan@gmail.com>
2015-10-02 11:48:01 +0200

    Finding my player

---
+++ spec/request_spec.rb
@@ -0,0 +1,60 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../player_service'
+require_relative '../player'
+require 'rspec'
+require 'rack/test'
+
+
+describe 'The HelloWorld App' do
+  include Rack::Test::Methods
+
+  let(:game_state) {
+      <<-JSON
+      {
+      "players": [
+        {
+          "name": "#{Player::PLAYER_NAME}",
+          "stack": 1000,
+          "status": "active",
+          "bet": 0,
+          "hole_cards": [],
+          "version": "Version name 1",
+          "id": 0
+        },
+        {
+          "name": "Player 2",
+          "stack": 1000,
+          "status": "active",
+          "bet": 0,
+          "hole_cards": [],
+          "version": "Version name 2",
+          "id": 1
+        }
+      ],
+      "tournament_id": "550d1d68cd7bd10003000003",
+      "game_id": "550da1cb2d909006e90004b1",
+      "round": 0,
+      "bet_index": 0,
+      "small_blind": 10,
+      "orbits": 0,
+      "dealer": 0,
+      "community_cards": [],
+      "current_buy_in": 0,
+      "pot": 0
+    }
+    JSON
+  }
+
+  def app
+    Sinatra::Application
+  end
+
+  it "says hello" do
+    post '/', action: 'bet_request', game_state: game_state
+
+    expect(last_response).to be_ok
+
+    p last_response.body
+  end
+end
