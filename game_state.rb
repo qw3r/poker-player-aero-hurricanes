95572d50fc639603344b8d55c2940f53bc7e15ee
Tamas Drahos <drahostamas@gmail.com>
2015-10-02 11:42:29 +0200

    Move code to GameState

--- game_state.rb
+++ game_state.rb
@@ -6,4 +6,24 @@ class GameState
   def [](key)
     @data[key]
   end
+
+  def has_pair?
+    my_player["hole_cards"].map { |card| card["rank"] }.uniq.size == 1
+  end
+
+  def my_player
+    @my_player ||= game_state['players'].select { |player| player['name'] == PLAYER_NAME }.first
+  end
+
+  def current_buy_in
+    @data['current_buy_in']
+  end
+
+  def small_blind
+    @data['small_blind']
+  end
+
+  def allin
+    my_player["stack"]
+  end
 end
--- player.rb
+++ player.rb
@@ -7,7 +7,7 @@ class Player
   PLAYER_NAME = 'Aero Hurricanes'
   RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-  attr_reader :my_player, :game_state
+  attr_reader :game_state
 
 
 
@@ -21,25 +21,12 @@ class Player
   def bet_request
     @logger.info game_state.inspect
 
-    buy_in = game_state['current_buy_in']
-    small_blind = game_state['small_blind']
-    @my_player = game_state['players'].select { |player| player['name'] == PLAYER_NAME }.first
-
-    return allin if has_pair?
+    buy_in = game_state.current_buy_in
+    small_blind = game_state.small_blind
+    
+    return game_state.allin if game_state.has_pair?
     return buy_in if (2 * small_blind >= buy_in)
     0
-  end
-
-
-
-  def has_pair?
-    @my_player["hole_cards"].map { |card| card["rank"] }.uniq.size == 1
-  end
-
-
-
-  def allin
-    @my_player["stack"]
   end
 
 
