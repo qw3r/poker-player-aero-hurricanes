e6104c50f00cb44711a03947ce7b24fa2a224f1e
Milán Unicsovics <u.milan@gmail.com>
2015-10-02 11:48:35 +0200

    Has pair improved.

--- game_state.rb
+++ game_state.rb
@@ -3,31 +3,51 @@ class GameState
     @data = data
   end
 
+
+
   def [](key)
     @data[key]
   end
 
+
+
   def has_pair?
-    my_player["hole_cards"].map { |card| card["rank"] }.uniq.size == 1
+    cards.map { |card| card["rank"] }.uniq.size < cards.size
   end
+
+
 
   def my_player
     @my_player ||= @data['players'][in_action]
   end
 
+
+
   def in_action
     @data['in_action']
   end
+
+
 
   def current_buy_in
     @data['current_buy_in']
   end
 
+
+
   def small_blind
     @data['small_blind']
   end
 
+
+
   def allin
     my_player["stack"]
+  end
+
+
+
+  def cards
+    @cards ||= my_player["hole_cards"] + @data["community_cards"]
   end
 end
