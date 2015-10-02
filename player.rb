818af8399f71ab0638c74785cd3cca536f182666
Milán Unicsovics <u.milan@gmail.com>
2015-10-02 10:24:19 +0200

    Get own player.

--- player.rb
+++ player.rb
@@ -4,8 +4,9 @@ require 'logger'
 class Player
 
   VERSION = "Default Ruby folding player"
-
-
+  PLAYER_NAME = 'Aero Hurricanes'
+  RANKS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  
 
   def initialize
     @logger = Logger.new $stdout
@@ -18,6 +19,7 @@ class Player
 
     buy_in = game_state['current_buy_in']
     small_blind = game_state['small_blind']
+    my_player = game_state['players'].select {|player| player['name'] == PLAYER_NAME }
 
     if 2 * small_blind <= buy_in
       buy_in
