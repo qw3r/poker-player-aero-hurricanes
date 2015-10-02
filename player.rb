1fecc03f96903c4c08e8fc757d76593200d016c6
Milán Unicsovics <u.milan@gmail.com>
2015-10-02 10:36:50 +0200

    Log player.

--- player.rb
+++ player.rb
@@ -5,8 +5,8 @@ class Player
 
   VERSION = "Default Ruby folding player"
   PLAYER_NAME = 'Aero Hurricanes'
-  RANKS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-  
+  RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
 
   def initialize
     @logger = Logger.new $stdout
@@ -20,6 +20,8 @@ class Player
     buy_in = game_state['current_buy_in']
     small_blind = game_state['small_blind']
     my_player = game_state['players'].select {|player| player['name'] == PLAYER_NAME }
+    @logger.info my_player
+    
 
     if 2 * small_blind >= buy_in
       buy_in
